0f91144d69063c63169e3c12a84e735f82d224dc
slsobel <sobelsl@gmail.com>
2021-04-23 11:26:53 -0400

    Set up components, views, services

---
+++ vue/src/components/Login.vue
@@ -0,0 +1,15 @@
+<!-- as component to show up as a pop-up window on homepage -->
+
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/NewUserForm.vue
@@ -0,0 +1,15 @@
+<!-- new user form as pop-up -->
+
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
